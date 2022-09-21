ccc69be43060ab4decc807babf0784bacbacc4d6
irteam <irteam@AL02284225.local>
2022-09-21 15:49:13 +0900

    init project

---
+++ src/main/java/com/jenkinsdockerslack/JenkinsDockerSlackApplication.java
@@ -0,0 +1,13 @@
+package com.jenkinsdockerslack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerSlackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerSlackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkinsdockerslack/JenkinsDockerSlackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkinsdockerslack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerSlackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
